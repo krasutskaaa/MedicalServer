f2836e60820737535ea3a704ff9de322473f68f0
krasutskaaa <krasutskadiana@gmail.com>
2024-08-08 20:37:37 +0300

    A record class with a name TestGeneralInfo was implemented

---
+++ Dtos/TestGeneralInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace CourseworkMedicalServer.Dtos;
+
+public record class TestGeneralInfo
+(
+    Guid PatientId,
+    Guid DoctorId,
+    float RedBloodCellCount,
+    float WhiteBloodCellCount,
+    float PlateletCount,
+    float HemoglobinTest,
+    float HematocritTest,
+    float MeanCorpuscularVolume
+    );
