e4850231ffea462a69733aa865a2b3f8cd343756
krasutskaaa <krasutskadiana@gmail.com>
2024-08-08 20:05:27 +0300

    A Test class was created

---
+++ Abstractions/Entities/Test.cs
@@ -0,0 +1,17 @@
+﻿namespace CourseworkMedicalServer.Abstractions.Entities;
+
+public class Test : Entity
+{
+    public Guid PatientId { get; set; }
+    public Guid DoctorId { get; set; }
+    public float RedBloodCellCount { get; set; }
+    public float WhiteBloodCellCount { get; set; }
+    public float PlateletCount { get; set; }
+    public float HemoglobinTest { get; set; }
+    public float HematocritTest { get; set; }
+    public float MeanCorpuscularVolume { get; set; }
+    public Doctor Doctor { get; set; }
+    public Patient Patient { get; set; }
+
+
+}
