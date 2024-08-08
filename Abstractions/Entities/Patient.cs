a963e9845c2a6102b8168f0550c880d9a6f1b056
krasutskaaa <krasutskadiana@gmail.com>
2024-08-08 20:03:20 +0300

    A Patient class was designed

---
+++ Abstractions/Entities/Patient.cs
@@ -0,0 +1,14 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace CourseworkMedicalServer.Abstractions.Entities;
+
+public class Patient : Entity, IAliveEntity
+{
+    public string Name { get; set; }
+    public Gender Gender { get; set; }
+    public DateOnly Birthday { get; set; }
+    public string? Description { get; set; }
+    public ICollection<Test> Tests { get; set; } = new List<Test>();
+
+
+}
