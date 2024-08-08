0c29346056df0d619c0889f43a767be4aa788c1c
krasutskaaa <krasutskadiana@gmail.com>
2024-08-08 20:28:38 +0300

    A record class with name PatientGeneralInfo was designed

---
+++ Dtos/PatientGeneralInfo.cs
@@ -0,0 +1,10 @@
+﻿using CourseworkMedicalServer.Abstractions;
+
+namespace CourseworkMedicalServer.Dtos;
+
+public record class PatientGeneralInfo(
+    string Name,
+    Gender Gender,
+    DateOnly Birthday,
+    string? Description
+    );
