571d7cbd6b152018575f530fd8bc83fa72a1f605
krasutskaaa <krasutskadiana@gmail.com>
2024-08-08 20:11:44 +0300

    A TestsStoreDBContext class was added to interact with a db

---
+++ Data/TestsStoreDBContext.cs
@@ -0,0 +1,29 @@
+﻿using CourseworkMedicalServer.Abstractions.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseworkMedicalServer.Data;
+
+public class TestsStoreDBContext(DbContextOptions<TestsStoreDBContext> options)
+    : DbContext(options)
+{
+    public DbSet<Doctor> Doctors => Set<Doctor>();
+    public DbSet<Patient> Patients => Set<Patient>();
+    public DbSet<Test> Tests => Set<Test>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        modelBuilder.Entity<Test>(entity =>
+        {
+            entity
+            .HasOne(test => test.Doctor)
+            .WithMany(doctor => doctor.Tests)
+            .HasForeignKey(test => test.DoctorId);
+
+            entity
+            .HasOne(test => test.Patient)
+            .WithMany(patient => patient.Tests)
+            .HasForeignKey(test => test.PatientId);
+        });
+    }
+}
