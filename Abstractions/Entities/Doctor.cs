c9dee9c38ac90f0a3a84cf45d7c865456d9789bf
krasutskaaa <krasutskadiana@gmail.com>
2024-08-08 20:00:47 +0300

    A class with name Doctor was implemented

---
+++ Abstractions/Entities/Doctor.cs
@@ -0,0 +1,33 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace CourseworkMedicalServer.Abstractions.Entities;
+
+public class Doctor : Entity, IAliveEntity
+{
+    public string Name { get; set; }
+    public Gender Gender { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public Specialization Specialization { get; set; }
+    public ICollection<Test> Tests { get; set; } = new List<Test>();
+}
+
+public enum Specialization
+{
+    Radiologist,
+    Surgeon,
+    Nephrologist,
+    Gynecologist,
+    Anesthesiologist,
+    Neurologist,
+    Oncologist,
+    Pathologist,
+    Psychiatrist,
+    Urologist,
+    Cardiologist,
+    Dentist,
+    Pulmonologist,
+    Ophthalmologist,
+    Pediatrician
+}
+
