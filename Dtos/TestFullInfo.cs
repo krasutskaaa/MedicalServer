7de99ff2a84e219b0923367d9c48de65631b92b0
krasutskaaa <krasutskadiana@gmail.com>
2024-08-08 20:36:00 +0300

    A record class with a name TestFullInfo was created

---
+++ Dtos/TestFullInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace CourseworkMedicalServer.Dtos;
+
+public record class TestFullInfo
+(
+    Guid Id,
+    Guid PatientId,
+    Guid DoctorId,
+    float RedBloodCellCount,
+    float WhiteBloodCellCount,
+    float PlateletCount,
+    float HemoglobinTest,
+    float HematocritTest,
+    float MeanCorpuscularVolume,
+    DateTime CreatedAt,
+    DateTime? UpdatedAt
+    );
