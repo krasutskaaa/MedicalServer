999ed613e46d5df469c37443237c5e8cc079675d
krasutskaaa <krasutskadiana@gmail.com>
2024-08-08 20:17:44 +0300

    A DoctorFullInfo record class was developed

---
+++ Dtos/DoctorFullInfo.cs
@@ -0,0 +1,15 @@
+﻿using CourseworkMedicalServer.Abstractions.Entities;
+using CourseworkMedicalServer.Abstractions;
+
+namespace CourseworkMedicalServer.Dtos;
+
+public record class DoctorFullInfo(
+
+    Guid Id,
+    string Name,
+    Gender Gender,
+    string Username,
+    Specialization Specialization,
+    DateTime CreatedAt,
+    DateTime? UpdatedAt
+    );
