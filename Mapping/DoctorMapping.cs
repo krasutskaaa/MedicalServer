b19c1b52424db5a5811145b7e2c0f58177f9acf1
krasutskaaa <krasutskadiana@gmail.com>
2024-08-08 20:45:29 +0300

    A static class with a name DoctorMapping was designed

---
+++ Mapping/DoctorMapping.cs
@@ -0,0 +1,59 @@
+﻿using CourseworkMedicalServer.Abstractions.Entities;
+using CourseworkMedicalServer.Dtos;
+
+namespace CourseworkMedicalServer.Mapping;
+
+public static class DoctorMapping
+{
+    public static DoctorGeneralInfo ToDoctorGeneralInfo(this Doctor doctor)
+    {
+        return new(
+            doctor.Name,
+            doctor.Gender,
+            doctor.Specialization
+            );
+    }
+
+    public static Doctor ToEntity(this CreateDoctorDto doctor)
+    {
+        return new Doctor()
+        {
+            Name = doctor.Name,
+            Gender = doctor.Gender,
+            Username = doctor.Username,
+            Password = doctor.Password,
+            Specialization = doctor.Specialization
+
+        };
+    }
+
+    public static DoctorFullInfo ToDoctorFullInfo(this Doctor doctor)
+    {
+        return new(
+            doctor.Id,
+            doctor.Name,
+            doctor.Gender,
+            doctor.Username,
+            doctor.Specialization,
+            doctor.CreatedAt,
+            doctor.UpdatedAt
+            );
+
+    }
+
+
+    public static Doctor ToEntity(this UpdateDoctorDto doctor, Guid id)
+    {
+        return new Doctor()
+        {
+            Id = id,
+            Name = doctor.Name,
+            Gender = doctor.Gender,
+            Username = doctor.Username,
+            Password = doctor.Password,
+            Specialization = doctor.Specialization,
+            UpdatedAt = DateTime.Now
+        };
+    }
+}
+
