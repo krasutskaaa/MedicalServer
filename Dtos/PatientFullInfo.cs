1ac8068c05cd728d9ef31f4b444ece2f7bbd3137
krasutskaaa <krasutskadiana@gmail.com>
2024-08-08 20:26:32 +0300

    A record class with name PatientFullInfo was developed

---
+++ Dtos/PatientFullInfo.cs
@@ -0,0 +1,14 @@
+﻿using CourseworkMedicalServer.Abstractions;
+
+namespace CourseworkMedicalServer.Dtos;
+
+public record class PatientFullInfo(
+
+    Guid Id,
+    string Name,
+    Gender Gender,
+    DateOnly Birthday,
+    string? Description,
+    DateTime CreatedAt,
+    DateTime? UpdatedAt
+    );
