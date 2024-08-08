6a4bbd004e22c47c9d5f9c3d5c19e27a50e6e3f3
krasutskaaa <krasutskadiana@gmail.com>
2024-08-08 20:33:30 +0300

    A CreateTestDto record class was appended

---
+++ Dtos/CreateTestDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseworkMedicalServer.Dtos;
+
+public record class CreateTestDto
+(
+    [Required]
+    Guid PatientId,
+    [Required]
+    Guid DoctorId,
+    [Required]
+    float RedBloodCellCount,
+    [Required]
+    float WhiteBloodCellCount,
+    [Required]
+    float PlateletCount,
+    [Required]
+    float HemoglobinTest,
+    [Required]
+    float HematocritTest,
+    [Required]
+    float MeanCorpuscularVolume
+
+    );
