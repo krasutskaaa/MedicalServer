d9ddf0cb975d77f40f23de1f8038ff747821cfca
krasutskaaa <krasutskadiana@gmail.com>
2024-08-08 20:24:32 +0300

    A CreatePatientDto record class was added

---
+++ Dtos/CreatePatientDto.cs
@@ -0,0 +1,15 @@
+﻿using CourseworkMedicalServer.Abstractions;
+using System.ComponentModel.DataAnnotations;
+
+namespace CourseworkMedicalServer.Dtos;
+
+public record class CreatePatientDto(
+    [Required][StringLength(50)]
+    string Name,
+    [Required]
+    Gender Gender,
+    [Required]
+    DateOnly Birthday,
+    string? Description
+
+);
