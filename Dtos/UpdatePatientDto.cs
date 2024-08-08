40a9bdea511ac996a91b3bedb8563ccf19a81e05
krasutskaaa <krasutskadiana@gmail.com>
2024-08-08 20:30:33 +0300

    An UpdatePatientDto record class was implemented

---
+++ Dtos/UpdatePatientDto.cs
@@ -0,0 +1,14 @@
+﻿using CourseworkMedicalServer.Abstractions;
+using System.ComponentModel.DataAnnotations;
+
+namespace CourseworkMedicalServer.Dtos;
+
+public record class UpdatePatientDto(
+    [Required][StringLength(50)]
+    string Name,
+    [Required]
+    Gender Gender,
+    [Required]
+    DateOnly Birthday,
+    string? Description
+);
