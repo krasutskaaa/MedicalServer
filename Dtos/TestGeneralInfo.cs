f6892c397527f369ecdd425b38040696f143c7d1
krasutskaaa <krasutskadiana@gmail.com>
2024-08-14 16:35:26 +0300

    The TestGeneralInfo record class was altered pursuant to the Test class

--- Dtos/TestGeneralInfo.cs
+++ Dtos/TestGeneralInfo.cs
@@ -4,10 +4,9 @@ public record class TestGeneralInfo
 (
     Guid PatientId,
     Guid DoctorId,
-    float RedBloodCellCount,
-    float WhiteBloodCellCount,
-    float PlateletCount,
-    float HemoglobinTest,
-    float HematocritTest,
-    float MeanCorpuscularVolume
+    string Name,
+    float Value,
+    float MinValue,
+    float MaxValue,
+    string UMeasurement
     );
