38b606d6689fae9bd8974f1114df812e01847015
krasutskaaa <krasutskadiana@gmail.com>
2024-08-16 16:57:22 +0300

    A new static class with a name DataExtensions was provided to update the db in a case of any migrations are added

---
+++ Data/DataExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CourseworkMedicalServer.Data;
+
+public static class DataExtensions
+{
+    public static async Task MigrateDbAsync(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<TestsStoreDBContext>();
+        await dbContext.Database.MigrateAsync();
+    }
+}
