86826ede242f76ea2d0ac458d976b4e8b9e50040
krasutskaaa <krasutskadiana@gmail.com>
2024-08-16 16:21:21 +0300

    The Doctor class was changed because of the IAliveEntity interface updates and the model simplification

--- Abstractions/Entities/Doctor.cs
+++ Abstractions/Entities/Doctor.cs
@@ -5,29 +5,12 @@ namespace CourseworkMedicalServer.Abstractions.Entities;
 public class Doctor : Entity, IAliveEntity
 {
     public string Name { get; set; }
-    public Gender Gender { get; set; }
+    public string Gender { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
-    public Specialization Specialization { get; set; }
+    public string Specialization { get; set; }
     public ICollection<Test> Tests { get; set; } = new List<Test>();
 }
 
-public enum Specialization
-{
-    Radiologist,
-    Surgeon,
-    Nephrologist,
-    Gynecologist,
-    Anesthesiologist,
-    Neurologist,
-    Oncologist,
-    Pathologist,
-    Psychiatrist,
-    Urologist,
-    Cardiologist,
-    Dentist,
-    Pulmonologist,
-    Ophthalmologist,
-    Pediatrician
-}
+
 
