461dc894e0b2eed52a2025a5338f7ff0cb6b5a6a
krasutskaaa <krasutskadiana@gmail.com>
2024-08-16 16:23:25 +0300

    The class with a name Patient was transformed according to the IAliveEntity interface changes

--- Abstractions/Entities/Patient.cs
+++ Abstractions/Entities/Patient.cs
@@ -5,7 +5,7 @@ namespace CourseworkMedicalServer.Abstractions.Entities;
 public class Patient : Entity, IAliveEntity
 {
     public string Name { get; set; }
-    public Gender Gender { get; set; }
+    public string Gender { get; set; }
     public DateOnly Birthday { get; set; }
     public string? Description { get; set; }
 
