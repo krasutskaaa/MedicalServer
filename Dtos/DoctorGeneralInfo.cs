4ca91d8f2dc76ae5cbb33143a05e45a8bdfe644b
krasutskaaa <krasutskadiana@gmail.com>
2024-08-25 21:56:45 +0300

    Deleted files were moved to a shared library

--- Dtos/DoctorGeneralInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using CourseworkMedicalServer.Abstractions.Entities;
-using CourseworkMedicalServer.Abstractions;
-
-namespace CourseworkMedicalServer.Dtos;
-
-public record class DoctorGeneralInfo(
-    string Name,
-    string Gender,
-    string Specialization);
