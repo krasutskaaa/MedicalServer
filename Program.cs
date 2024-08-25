e19487b3766a14e30aeca91301bad0da7dccca94
krasutskaaa <krasutskadiana@gmail.com>
2024-08-25 21:28:43 +0300

    The Program class was connected to endpoints and a db migration method was applied

--- Program.cs
+++ Program.cs
@@ -5,5 +5,9 @@ var builder = WebApplication.CreateBuilder(args);
 var connString = builder.Configuration.GetConnectionString("TestsStore");
 builder.Services.AddSqlite<TestsStoreDBContext>(connString);
 var app = builder.Build();
+app.MapDoctorsEndpoints();
+app.MapPatientsEndpoints();
+app.MapTestsEndpoints();
+await app.MigrateDbAsync();
 
 app.Run();
